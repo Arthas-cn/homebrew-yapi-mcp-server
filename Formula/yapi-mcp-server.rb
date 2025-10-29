88e59a8f0424534da50df5973611f8f7eb158583
Arthas_cn <zhouzhiguan@mqjc.com>
2025-10-29 20:29:08 +0800

    Update SHA-256 for v1.0.0

--- Formula/yapi-mcp-server.rb
+++ Formula/yapi-mcp-server.rb
@@ -8,7 +8,7 @@ class YapiMcpServer < Formula
   desc "Yapi MCP Server - Swift实现的Yapi MCP工具"
   homepage "https://github.com/Arthas-cn/yapi-mcp-server-releases"
   url "https://github.com/Arthas-cn/yapi-mcp-server-releases/releases/download/v1.0.0/YapiMCPServer-macos.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "cee958db0b04b6e8c3bd777828ebf861ddfd572c7df3b162df7bf959a6f49082"
   version "1.0.0"
   
   depends_on :macos
